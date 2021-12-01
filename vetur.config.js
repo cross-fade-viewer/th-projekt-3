2091bd862f6dd023c0e2bf5fcfdf6d5f8aaa33f0
Yannic Brügger <yannic.bruegger@gmail.com>
2021-12-01 15:49:23 +0100

    refactor: :recycle: remove unused code

--- vetur.config.js
+++ vetur.config.js
@@ -1,14 +1,7 @@
 // vetur.config.js
 /** @type {import('vls').VeturConfig} */
 module.exports = {
-  settings: {
-    "vetur.useWorkspaceDependencies": true,
-    "vetur.experimental.templateInterpolationService": true
-  },
   projects: [
-    './packages/repo2', // Shorthand for specifying only the project root location
-    {
-      root: './vue-component',
-    }
+    './vue-component'
   ]
 }
